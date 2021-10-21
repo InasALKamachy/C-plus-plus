b025b7eebd8a3327b9b7f902f6d32864629b049a
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-21 09:43:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ line*/
 #include<iostream>  // #include is preprocessor and iostream is library for in and output 
 
 main(){
+ const int weightGoal = 100; //the const can not be change at all!!
  std::cout<<"hello world"; // or add  using namespace std; after # include
  
  return 0;

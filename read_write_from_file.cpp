6398fd8770289a4cb593e97e75f12f8ac76f3a57
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-21 11:51:59 +0300

    Create read_write_from_file.cpp

---
+++ read_write_from_file.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream> // for read and write
+#include <string>
+using namespace std;
+
+int main () {
+    string line;
+    //create an output stream to write to the file
+    //append the new lines to the end of the file
+    ofstream myfileI ("input.txt", ios::app); //ofstream is for write on the file
+    if (myfileI.is_open())
+    {
+        myfileI << "\nI am adding a line.\n";
+        myfileI << "I am adding another line.\n";
+        myfileI.close();
+    }
+    else cout << "Unable to open file for writing";
+
+    //create an input stream to read the file
+    ifstream myfileO ("input.txt"); // ifstream is for read file
+    //During the creation of ifstream, the file is opened. 
+    //So we do not have explicitly open the file. 
+    if (myfileO.is_open())
+    {
+        while ( getline (myfileO,line) )
+        {
+            cout << line << '\n';
+        }
+        myfileO.close();
+    }
+
+    else cout << "Unable to open file for reading";
+
+    return 0;
+}

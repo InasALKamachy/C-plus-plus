88fff2236d8cec100c106bd27e70aaedcbf4154f
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-21 09:25:47 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// the first program in c++
+/* here is the comments for multi
+line*/
+
+#include<iostream>  // #include is preprocessor and iostream is library for in and output 
+
+main(){
+ std::cout<<"hello world"; // or add  using namespace std; after # include
+ 
+ return 0;
+}
+

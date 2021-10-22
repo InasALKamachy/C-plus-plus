161328a888c9c9cf12d610b114ac2ba012f9218d
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-10-22 16:31:27 +0300

    Update calculate_area_of_room.cpp

--- calculate_area_of_room.cpp
+++ calculate_area_of_room.cpp
@@ -9,29 +9,37 @@
  */
 
  
+ /*Goal: practice getting string inputs and 
+ **converting them to numeric variables using
+ **stringstream.
+ **
+ **Prompt the user for the length of a room. 
+ **Then prompt for the width of the room.
+ **Print out the area of the room. 
+ */
+
  #include <iostream>
  #include <string>
  #include <sstream>
 
  int main ()
  {
-   std::string width;
-   std::string length;
-   float w_inches = 0;
-   float l_inches = 0;
-   float room_area = 0;
+   std::string stringLength, stringWidth;
+   float length = 0;
+   float width = 0;
+   float area = 0;
 
-   std::cout << "Enter the width of the room: ";
-   std::getline (std::cin,width);
-   std::stringstream(width) >> w_inches;
-   std::cout<<"You entered "<<w_inches<<"\n";
-   
    std::cout << "Enter the length of the room: ";
-   std::getline (std::cin,length);
-   std::stringstream(length) >> l_inches;
-   std::cout<<"You entered "<<l_inches<<"\n";
-   
-   room_area = w_inches * l_inches;
-   std::cout << room_area;
+   //get the length as a string
+   std::getline (std::cin,stringLength);
+   //convert to a float
+   std::stringstream(stringLength) >> length;
+   //get the width as a string
+   std::cout << "Enter width: ";
+   std::getline (std::cin,stringWidth);
+   //convert to a float
+   std::stringstream(stringWidth) >> width;
+   area = length * width;
+   std::cout << "\nThe area of the room is: " << area << std::endl;
    return 0;
  }

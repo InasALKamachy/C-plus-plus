4e89f5157707127fb9bf8adaa03096ed126787fa
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-01-31 23:43:07 +0300

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x;
+cout<<"Enter your number"<<endl;
+cin>>x;
+switch(x){
+case 1:
+cout<<"January"<<endl;
+break;
+case 2:
+cout<<"Febreuary"<<endl;
+break;
+default:
+cout<<"end";}
+cin.get();
+
+}

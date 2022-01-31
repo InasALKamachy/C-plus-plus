4deea9883da29f16c473b2d56543e57c8ee5e0f0
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-01-31 21:00:59 +0300

    Create hypontenous.cpp

---
+++ hypontenous.cpp
@@ -0,0 +1,15 @@
+#include<cmath>
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    float side1,side2;
+ cout<<"Enter your X" <<endl;
+ cin>>side1;
+ cout<<"Enter Y"<<endl;
+ cin>>side2;
+ 
+ cout<<"The square of hypotenous is : "<<sqrt(pow(side1,2)+pow(side2,2));
+}//(side1 * side1) + (side2 * side2)
+ 

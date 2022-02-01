6b03d5a0c9afb333335a7e64920210e80f595436
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-02-01 13:50:13 +0300

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double fact(1);
+    int n(1);
+    cout<<"Enter your number for multiplay:"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++){ //rows
+    
+    fact *=i;
+    
+ 
+ 
+      
+}
+cout<<fact<<endl;
+cin.get();
+}

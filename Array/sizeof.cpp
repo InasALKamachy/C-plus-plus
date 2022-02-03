546215c5173f9f4e36b3c48d060ae5d8ce601707
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-02-03 18:40:18 +0300

    Create sizeof.cpp

---
+++ Array/sizeof.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+char myArray[] = {'a','b','c','d'};
+char myArray1[]="abcd";
+
+for (int i=0; i<sizeof(myArray);i++){
+    cout<<myArray[i]<<',';
+    
+}
+cout<<endl<<endl;
+
+for (int i=0; i<sizeof(myArray1);i++){
+    cout<<myArray1[i]<<',';
+}
+cin.get();
+}

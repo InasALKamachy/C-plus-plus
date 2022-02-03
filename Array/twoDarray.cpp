637ae62726dfd313ff663d71cd1d5fc1d8be847c
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-02-03 21:06:23 +0300

    Create twoDarray.cpp

---
+++ Array/twoDarray.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row =2;
+    int col = 3;
+   int myArray[row][col] = {{1,2,3},{11,22,33},{222,333,444}};
+   for (int i=0;i<=row;i++){
+       for(int j=0;j<=col;j++){
+ cout<<myArray[i][j]<<'|';
+       }
+      cout<<endl;
+       
+   }
+
+    return 0;
+}

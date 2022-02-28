eb8f631c49af6d3d0c97a9f12b0bb3c696bd1b69
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-02-28 02:54:30 -0800

    Create refrence.cpp

---
+++ refrence.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+	int y = 5;
+	int& p = y;
+	cout << y <<"\t"<<sizeof(y) << endl;
+	cout << p <<sizeof(p)<< endl;
+	int x = 7;
+	
+
+
+	system("pause");
+	return 0;
+}

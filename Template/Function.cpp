fd5a0b00f68f6626ba7268f0ae15535007d65add
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-03-03 03:37:03 -0800

    Create Function.cpp

---
+++ Template/Function.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+template <class T>
+T time10(T num){
+    return 10*num;
+}
+int main() {
+    
+    
+    cout<<time10()<<endl;
+    return 0;
+}

738e856e2756b8cea2eae7b1cf6b59f4cf752f97
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-03-08 04:51:55 -0800

    Create Function.cpp

--- Template/Function.cpp
+++ Template/Function.cpp
@@ -1,3 +1,5 @@
+
+   
 // Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
@@ -6,9 +8,17 @@ template <class T>
 T time10(T num){
     return 10*num;
 }
+template <typename T1, typename T2>
+T1 max(T1 num1, T2 num2){
+    if (num1> num2) return num1;
+    else return num2;
+}
 int main() {
     
-    
-    cout<<time10()<<endl;
+   
+    cout<<max(2.000,255*10)<<endl;
+    cout<<time10(-0.2)<<endl;
     return 0;
 }
+
+////////////

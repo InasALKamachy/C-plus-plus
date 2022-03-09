e586182e7b553aa58f30186576c607e3c443f68c
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-03-08 20:41:43 -0800

    Update print_class_as function.cpp

--- Classe/print_class_as function.cpp
+++ Classe/print_class_as function.cpp
@@ -24,3 +24,38 @@ int main(){
     my_day.printday(my_day);
     return 0;
 }
+
+/////////////////////////
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Car {
+  public:
+    string brand;   
+    string model;
+    int year;
+    void type(Car d){
+         cout << d.brand << " " << d.model << " " << d.year <<endl;
+    }
+};
+
+int main() {
+  // Create an object of Car
+  Car carObj1;
+  carObj1.brand = "BMW";
+  carObj1.model = "X5";
+  carObj1.year = 1999;
+
+  // Create another object of Car
+  Car carObj2;
+  carObj2.brand = "Ford";
+  carObj2.model = "Mustang";
+  carObj2.year = 1969;
+
+  // Print attribute values
+  carObj1.type(carObj1);
+  carObj2.type(carObj2);
+  return 0;
+}

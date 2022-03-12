9d5b571875b2c5d93a8befbe6c8376b424a6df68
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-03-11 22:25:57 -0800

    Update Test_1.cpp

--- Google_Test/Test_1.cpp
+++ Google_Test/Test_1.cpp
@@ -10,6 +10,8 @@ TEST(TestCaseName, TestName) {
 	total += value;
 	cout << total << "//" << value << endl;
   EXPECT_EQ(total, value);
+  //ASSERT_TRUE, ASSERT_FALSE,ASSERT_NE(1,1), ASSERT_LT(1,2), ASSERT_LE(2,3), ASSERT_GT(3,1), ASSERT_GE(3,2), ASSERT_STREQ("E","E"), ASSERT_STRNQ("E","O")
+  // SAME AS EXPECT , NE= NOT EQUAL, LT= LESS THAN, GT= GREATER THAN, GE= GREATER TAHN AND EQUAL, 
   
   cout << "Thank you your value was entered!!" << endl;
 }

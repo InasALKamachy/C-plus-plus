9693b483dea1af07fd7f1d10cacbfb7eb1076dbe
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-07-12 20:49:49 +0300

    Update rand.cpp

--- Function/rand.cpp
+++ Function/rand.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<cstdlib>
+#include<ctime>
 using namespace std;
 
 
 int main(){
-srand(1);
+srand(time(0));//give different value every time
 for(int i=0;i<100;i++){
     cout<<rand()%(30-20+1)+20<<endl;
 }

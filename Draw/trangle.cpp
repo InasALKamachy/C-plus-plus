d151809a26709b0049da39cb115338b3baa136a1
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-07-20 20:48:53 +0300

    Create trangle.cpp

---
+++ Draw/trangle.cpp
@@ -0,0 +1,49 @@
+/*
+   *
+  ***
+ *****
+ 
+
+*/
+/**
+*    5 row
+**
+***
+****
+*****  5 column 
+/////////////////
+    *
+   ****
+  ******
+ ********
+**********
+
+
+*****
+****
+***
+**
+*
+
+////*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int e=1;
+    for(int i=1;i<=5;i++){  //rows  
+        for(int j=4;j>=i;j--){
+            cout<<" ";
+        }
+        for(int k=0;k<e;k++){
+            cout<<"*";
+        }
+       
+        cout<<endl;
+        e+=2;
+    }
+   
+    
+	return 0;
+}

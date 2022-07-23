96cef9b55d6bc9686ba3c8df722b623e50d64966
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-07-23 06:27:06 +0300

    Update reverse.cpp

--- Array/reverse.cpp
+++ Array/reverse.cpp
@@ -1,4 +1,6 @@
 
+
+//print reversed array without actually reverse it!!
 #include <iostream>
 using namespace std;
 #include<utility>
@@ -18,7 +20,32 @@ int main()
 
 
 
-
+/////////// reverse arrary 
+#include<iostream>
+#include<utility>
+using namespace std;
+int main()
+{
+    int arr[100], tot, i, j, temp;
+    cout << "Enter the Size for Array: ";
+    cin >> tot;
+    cout << "Enter " << tot << " Array Elements: ";
+    for (i = 0; i < tot; i++)
+        cin >> arr[i];
+    cout << "\nThe Original Array is:\n";
+    for (i = 0; i < tot; i++)
+        cout << arr[i] << " ";
+    j = tot - 1;
+    for (i = 0; i < j; i++, j--)
+    {
+        swap(arr[i],arr[j]);
+    }
+    cout << "\n\nThe Reverse of Given Array is:\n";
+    for (i = 0; i < tot; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
 
 
 

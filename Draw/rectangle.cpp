5f810e0c20096e366dc5f86e18406355c67c6dce
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-07-25 02:32:15 +0300

    Update rectangle.cpp

--- Draw/rectangle.cpp
+++ Draw/rectangle.cpp
@@ -30,3 +30,38 @@ int main()
    
     return 0;
 }
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<endl;
+    
+    for(int i=1;i<=10;i++){
+       for(int j = 1;j<=10;j++){
+         
+           if(i==1||i==10){
+               cout<<"*";
+           }
+           else if(i==j){
+             cout<<"*";
+           }
+           else if(i+j==10){
+             cout<<"*";
+           }
+           else if(j==1||j==10){
+               cout<<"*";
+           }
+           else cout<<" ";
+           
+       }
+       cout<<endl;
+    }
+   
+    return 0;
+}

5f038e5cfd57904daf429d81504f5178d1653103
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-08-12 23:40:27 +0300

    Create friendOperator.cpp

---
+++ Classe/friendOperator.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+class Unray{
+private:
+          int x,y,l;
+public:
+          Unray(int x=0, int y=0, int l=0){
+                    this->x = x;
+                    this->y = y;
+                    this->l = l;
+          }
+
+          void show(){
+          cout<<x<< " - "<<y<<" -"<<l<<endl;
+          }
+          Unray operator+(Unray d){
+          Unray b;
+          b.x = x+d.x;
+          b.y = y+d.y;
+          b.l = l+d.l;
+          return b;
+          }
+          Unray operator+(int d){
+          Unray b;
+          b.x = x+d;
+          b.y = y+d;
+          b.l = l+d;
+          return b;
+          }
+
+
+
+         bool operator==(Unray b){
+         return(x==b.x&&y==b.y&&l==b.l);
+         }
+         friend Unray operator+(int d, Unray b);
+          ~Unray(){
+          cout<<"DEconstructure"<<endl;
+          }
+
+};
+          Unray operator+(int d, Unray b){
+          Unray c;
+          c.x = d + b.x;
+          c.y = d + b.y;
+          c.l = d + b.l;
+          return c;
+          }
+int main(){
+Unray c1(3,1), c2(1,2), c3;
+c3 = c1+c2;
+c3.show();
+c3 = c1+10;
+c3.show();
+c3 = 10+c1;
+c3.show();
+
+
+
+
+Unray s,s1,s2;
+
+
+return 0;
+}

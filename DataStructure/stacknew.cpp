392650156fd89eb62171ed2adbb5e8e31621a938
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-08-21 16:32:00 +0300

    Update stacknew.cpp

--- DataStructure/stacknew.cpp
+++ DataStructure/stacknew.cpp
@@ -8,91 +8,88 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <iostream>
 using namespace std;
+
 const int Max_size = 5;
+
+
 class stack{
-
-    private:
-    int val;
-    int top;
+private:
+    int val,top;
     int arr[Max_size];
-    public:
+public:
     stack(){
-        top = -1;
-}
-void push(int item){
-    if(top >=Max_size-1){
-        cout<<"Your Stack is Full!\n";
-    }else{
-        top++;
-        arr[top] = item;
+    cout<<"Your stack is created!\n";
+    top = -1;
     }
+    void push(int item);///***
+    void pop();
+    void pop(int &item);
+    bool is_Empty();
+    bool is_full();
+    void show();
 
-}
-void pop(){
-    if(is_Empty())
-        cout<<"Your Stack is Empty!!\n";
-else
-         top--;
-
-
-}
-
-void pop(int &item){
-    if(is_Empty())
-        cout<<"Your Stack is Empty!!\n";
-else{
-
-    item = arr[top];
-}
-         top--;
-
-
-}
-void getTop(){
-if(is_Empty())
-    cout<<"Stack is Empty\n";
-else{
-
-    cout<<"The Last Element is : "<<top<<endl;
-}
-}
-bool is_Empty(){
-    return (top<0);
-}
-bool is_full(){
-    return top == val;
-}
-void show(){
-for(int i = Max_size-1;i>=0;i--)
-
-    cout<<"The\t"<<Max_size - i<<"\t"<<"Element is"<<":\t"<<arr[i]<<endl;
-
-
-}
 };
+void stack::push(int item){
+if(stack::is_full()){
+    cout<<"Your Stack is full!\n";
+}
+else {
+    top++;
+    arr[top] = item;
+}}
+void stack::show(){
+for(int i = top;i>=0;i--){
+ cout<<"[";
+    cout<<arr[i]<<" ";
+cout<<"]";
+
+}
+}
+
+void stack::pop(int &item){
+if(stack::is_Empty()){
+    cout<<"Your stack is Empty!\n";
+}
+else{
+    item = arr[top];
+    top--;
+}
+}
+void stack::pop(){
+if(stack::is_Empty()){
+    cout<<"Your stack is Empty!\n";
+}
+else{
+
+   top--;
+}
+}
+bool stack::is_Empty(){
+return top<0;
+}
+bool stack::is_full(){
+return (top>=Max_size-1);
+
+}
 int main(){
 stack ob;
-
-ob.push(0);
-ob.push(1);
 ob.push(2);
-ob.push(3);
-ob.push(4);
-
-cout<<"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"<<endl;
-//for(int i = 0;i<6;i++){
-//    ob.pop();
-//}
-
-//for(int i = 0;i<8;i++){
-//    ob.push(5);
-//}
+ob.push(52);
+ob.push(55);
+ob.push(24);
+ob.push(10);
 
 ob.show();
-ob.getTop();
+cout<<endl;
+cout<<"After pop"<<endl;
+ob.pop();
+ob.pop();
+ob.pop();
+ob.pop();
+ob.show();
 
 
-    return 0;
+
+
+return 0;
 }
-
-

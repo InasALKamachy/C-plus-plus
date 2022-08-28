1bc19db741066f8044e7f00780f14de335999f24
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-08-28 12:14:56 +0300

    Create stack.cpp

---
+++ BeautifulCode/stack.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include<stack>
+#include<queue> 
+using namespace std;
+
+void show(stack<int>s){
+    
+    while(!s.empty()){
+        cout<<s.top()<<"";
+        s.pop();
+    }
+}
+int main(){
+    
+    stack<int>s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.pop();
+   // cout<<s.size();
+    //cout<<s.empty();
+    show(s);
+    
+    return 0;
+}

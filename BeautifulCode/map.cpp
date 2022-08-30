6d4b65310f4e3079610beb57ed963590cce378d2
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-08-30 11:07:35 +0300

    Update map.cpp

--- BeautifulCode/map.cpp
+++ BeautifulCode/map.cpp
@@ -7,24 +7,27 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <iostream>
-#include<stack>
-#include<queue>
-#include<map> // we can used unordered_map for unorder in alphabetic and number 
+#include<map>
+#include<list>
 #include<string>
 using namespace std;
 
-
-int main(){
-
-   map<string,string>Dictionary;
-   Dictionary.insert(pair<string, string>("apple", "elma"));
-   Dictionary.insert(pair<string, string>("mother", "anni"));
-   Dictionary.insert(pair<string, string>("yes", "evit"));
-   Dictionary.insert(pair<string, string>("no", "hair"));
-
-   for(auto pair:Dictionary){
-    cout<<pair.first<<"-"<<pair.second<<endl;
+int main()
+{
+   list<string>machineLearning={"CNN"," ANN"," FNN"};
+   list<string>webDevelopment = {"HTML", " CSS"," JAVASCRIPT"," PHP"," ARAVEL"};
+   list<string>mobileApp = {"Dart", " Flutter"," androidStudio"};
+   map<string,list<string>>SeniorProgrammer;
+   SeniorProgrammer.insert(pair<string,list<string>>("ML",machineLearning));
+   SeniorProgrammer.insert(pair<string,list<string>>("web",webDevelopment));
+   SeniorProgrammer.insert(pair<string,list<string>>("mobile",mobileApp));
+   
+   for(auto pair:SeniorProgrammer){
+       cout<<pair.first <<" --";
+       for(auto attak:pair.second)
+       cout<<attak;
+       cout<<endl;
    }
-
-    return 0;
+   
+   return 0;
 }

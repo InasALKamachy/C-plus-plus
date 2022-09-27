bb6867a3c7f8500c00f392504da215f95dcd7e0b
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2022-09-27 20:54:57 +0300

    Update sort.cpp

--- Vectors/sort.cpp
+++ Vectors/sort.cpp
@@ -31,3 +31,34 @@ int main()
 
     return 0;
 }
+
+
+
+****************************
+
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main()
+{
+int x = 8;
+    vector<int> v = {34,3,22,5,6};
+    sort(v.begin(), v.end(), greater<int>());
+    cout<<"SORTED"<<endl;
+    for(auto x:v){
+        cout<<x<<" ";
+    }
+   
+    return 0;
+}
